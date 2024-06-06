275119527381a55cc7dceb4f1951bc771ba4841d
DonLemos <donovangarethlemos@gmail.com>
2024-06-06 19:49:51 +0200

    update

--- Expense-Tracker/expense_tracker/src/pages/expense-tracker/index.jsx
+++ Expense-Tracker/expense_tracker/src/pages/expense-tracker/index.jsx
@@ -1,105 +1,136 @@
 import { useState } from "react";
+import { signOut } from "firebase/auth";
 import { useAddTransaction } from "../../hooks/useAddTransaction";
 import { useGetTransactions } from "../../hooks/useGetTransactions";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { useNavigate } from "react-router-dom";
 
-import "./styles.css"
-
+import "./styles.css";
+import { auth } from "../../config/firebase-config";
 
 export const ExpenseTracker = () => {
+  const { addTransaction } = useAddTransaction();
+  const { transactions, transactionTotals } = useGetTransactions();
+  const { name, profilePhoto } = useGetUserInfo();
+  const navigate = useNavigate();
 
-    const { addTransaction } = useAddTransaction();
-    const { transactions } = useGetTransactions();
+  const [description, setDescription] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("expense");
 
-    const [description, setDescription] = useState("");
-    const [transactionAmount, setTransactionAmout] = useState(0);
-    const [transactionType, setTransactionType] = useState("expense");
+  const { balance, income, expenses } = transactionTotals;
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        addTransaction({
-            description,
-            transactionAmount,
-            transactionType,
-        });
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTransaction({
+      description,
+      transactionAmount,
+      transactionType,
+    });
 
+    setDescription("");
+    setTransactionAmount("");
+  };
 
-    return (
-        <>
-            <div className="expense-tracker">
-                <div className="container">
-                    <h1> Expense Tracker</h1>
-                    <div className="Balance">
-                        <h3>Your Balance</h3>
-                        <h2>R0.00</h2>
-                    </div>
-                    <div className="Summary">
-                        <div className="Income">
-                            <h4>Income</h4>
-                            <p>R0.00</p>
-                        </div>
-                        <div className="Expenses">
-                            <h4>Expenses</h4>
-                            <p>R0.00</p>
-                        </div>
-                    </div>
-                    <form className="Add-Transaction" onSubmit={onSubmit}>
-                        <input type="text" placeholder="Description" required onChange={(e) => setDescription(e.target.value)} />
-                        <input type="number" placeholder="Amount" required
-                            onChange={(e) => setTransactionAmout(e.target.value)} />
+  const signUserOut = async () => {
+    try {
+      await signOut(auth);
+      localStorage.clear();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-
-                        <input
-                            type="radio"
-                            id="Expense"
-                            value="expense"
-                            checked={transactionType === "expense"}
-                            onChange={(e) => setTransactionType(e.target.value)}
-                        />
-                        <label htmlFor="expense"> Expense</label>
-
-                        <input
-                            type="radio"
-                            id="Income"
-                            value="income"
-                            checked={transactionType === "income"}
-                            onChange={(e) => setTransactionType(e.target.value)}
-                        />
-                        <label htmlFor="income"> Income</label>
-
-                        <button type="submit"> Add Transaction</button>
-                    </form>
-                </div>
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1> {name}'s Expense Tracker</h1>
+          <div className="balance">
+            <h3> Your Balance</h3>
+            {balance >= 0 ? <h2> ${balance}</h2> : <h2> -${balance * -1}</h2>}
+          </div>
+          <div className="summary">
+            <div className="income">
+              <h4> Income</h4>
+              <p>${income}</p>
             </div>
-
-
-            <div className="transactions">
-                <h3> Transactions</h3>
-                <ul>
-                    {transactions.map((transaction) => {
-                        const { description, transactionAmount, transactionType } =
-                            transaction;
-
-                        return (
-                            <li>
-                                <h4> {description} </h4>
-                                <p>
-                                    {" "}
-                                    ${transactionAmount} • <label style={{
-                                        color: transactionType === "expense" ? "red" : "green",
-                                    }}
-                                    >
-                                        {" "}
-                                        {transactionType}{" "}
-                                    </label>
-                                </p>
-                            </li>
-                        );
-                    })}
-                </ul>
+            <div className="expenses">
+              <h4> Expenses</h4>
+              <p>${expenses}</p>
             </div>
-        </>
-    );
+          </div>
+          <form className="add-transaction" onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Description"
+              value={description}
+              required
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Amount"
+              value={transactionAmount}
+              required
+              onChange={(e) => setTransactionAmount(e.target.value)}
+            />
+            <input
+              type="radio"
+              id="expense"
+              value="expense"
+              checked={transactionType === "expense"}
+              onChange={(e) => setTransactionType(e.target.value)}
+            />
+            <label htmlFor="expense"> Expense</label>
+            <input
+              type="radio"
+              id="income"
+              value="income"
+              checked={transactionType === "income"}
+              onChange={(e) => setTransactionType(e.target.value)}
+            />
+            <label htmlFor="income"> Income</label>
+
+            <button type="submit"> Add Transaction</button>
+          </form>
+        </div>
+        {profilePhoto && (
+          <div className="profile">
+            {" "}
+            <img className="profile-photo" src={profilePhoto} />
+            <button className="sign-out-button" onClick={signUserOut}>
+              Sign Out
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="transactions">
+        <h3> Transactions</h3>
+        <ul>
+          {transactions.map((transaction) => {
+            const { description, transactionAmount, transactionType } =
+              transaction;
+            return (
+              <li>
+                <h4> {description} </h4>
+                <p>
+                  ${transactionAmount} •{" "}
+                  <label
+                    style={{
+                      color: transactionType === "expense" ? "red" : "green",
+                    }}
+                  >
+                    {" "}
+                    {transactionType}{" "}
+                  </label>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
-
-
